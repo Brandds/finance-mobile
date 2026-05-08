3cb15231339b4b4a7910908db37b295d7ed91469
Rafael Brandao <rbs13bg13@gmail.com>
2026-05-07 22:52:50 -0300

    criado componente homescreen

--- src/features/home/screens/HomeScreen.tsx
+++ src/features/home/screens/HomeScreen.tsx
@@ -1,5 +1,7 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaViewBase } from 'react-native';
 import { useTheme } from '../../../theme/useTheme';
+import { SafeAreaView } from "react-native-safe-area-context";
+
 import { Button } from '@/components';
 
 export default function Home() {
@@ -11,31 +13,9 @@ export default function Home() {
   }
 
   return (
-    <View
-      style={[
-        styles.container,
-        { backgroundColor: theme.colors.background },
-      ]}
-    >
-      <Text
-        style={[
-          theme.typography.h1,
-          { color: theme.colors.text },
-        ]}
-      >
-        Saldo Total
-      </Text>
-
-      <Text
-        style={[
-          theme.typography.display,
-          { color: theme.colors.primary, marginTop: theme.spacing.sm },
-        ]}
-      >
-        R$ 2.450,00
-      </Text>
-      <Button title="Adicionar Transação" variant="secondary" onPress={handleAddTransaction} />
-    </View>
+    <SafeAreaView>
+      
+    </SafeAreaView>
   )
 }
 
