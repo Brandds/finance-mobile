50aae585d2121ac7847b74ea85734451655ecb88
Rafael Brandao <rbs13bg13@gmail.com>
2026-05-07 22:50:45 -0300

    criado tela de login

---
+++ src/features/auth/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import Screen from "@/components/Screen/Screen";
+import Typography from "@/components/Typography/Typography";
+
+export default function LoginScreen() {
+    return (
+        <Screen>
+            <Typography variant="h1">
+                Dashboard
+            </Typography>
+        </Screen>
+    )
+}
