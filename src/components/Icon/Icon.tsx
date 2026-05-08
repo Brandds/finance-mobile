7878f7c5ee13029deef72f10916d5f6833b5cd18
Rafael Brandao <rbs13bg13@gmail.com>
2026-05-07 23:58:42 -0300

    modificado componente Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { MaterialIcons } from '@expo/vector-icons'
 import { useTheme } from '@/theme/useTheme'
+import { View } from 'react-native'
 
 type Props = {
   name: keyof typeof MaterialIcons.glyphMap
   size?: number
   color?: string
+  styleView?: object
 }
 
-export const Icon = ({ name, size = 24, color }: Props) => {
+export const Icon = ({ name, size = 24, color, styleView }: Props) => {
   const theme = useTheme()
 
   return (
-    <MaterialIcons
-      name={name}
-      size={size}
-      color={color || theme.colors.textSecondary}
-    />
+    <View style={styleView}>
+      <MaterialIcons
+        name={name}
+        size={size}
+        color={color || theme.colors.textSecondary}
+      />
+    </View>
   )
 }
