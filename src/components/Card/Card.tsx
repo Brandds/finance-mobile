e16d783a944a757e9f79d54ecd17ea3ecaf4f52f
Rafael Brandao <rbs13bg13@gmail.com>
2026-05-07 23:58:25 -0300

    modificado componente card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,19 +2,12 @@ import React from 'react'
 import { View } from 'react-native'
 import { useTheme } from '@/theme/useTheme'
 
-export const Card = ({ children }: { children: React.ReactNode }) => {
+export const Card = ({ children, styleCard }: { children: React.ReactNode; styleCard: object }) => {
   const theme = useTheme()
 
   return (
     <View
-      style={{
-        backgroundColor: '#fff',
-        padding: theme.spacing.md,
-        borderRadius: theme.radius.lg,
-        borderWidth: 0.5,
-        borderColor: theme.colors.border,
-        ...theme.shadows.level1,
-      }}
+      style={styleCard}
     >
       {children}
     </View>
