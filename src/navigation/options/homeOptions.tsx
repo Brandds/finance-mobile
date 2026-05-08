7b73c72db91672fc230ed86247c58b11b587a3a1
Rafael Brandao <rbs13bg13@gmail.com>
2026-05-07 21:21:09 -0300

    modificado options do home

--- src/navigation/options/homeOptions.tsx
+++ src/navigation/options/homeOptions.tsx
@@ -1,34 +1,5 @@
-import { tokens } from "@/theme/tokens";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
 const homeOptions = {
-  header: () => (
-    <SafeAreaView
-      edges={["top"]}
-      style={{
-        backgroundColor: tokens.colors.primary,
-        ...tokens.shadows.level1,
-      }}
-    >
-      <View
-        style={{
-          height: 80,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            color: tokens.colors.onPrimary,
-            ...tokens.typography.h1,
-          }}
-        >
-          Finance App
-        </Text>
-      </View>
-    </SafeAreaView>
-  ),
+  headerShown: false,
 };
 
 export default homeOptions;
