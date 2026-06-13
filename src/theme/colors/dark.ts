43f9223f57bf35d96699bdbb7f5539f7a5656fb4
Rafael Brandao <rbs13bg13@gmail.com>
2026-06-13 20:02:30 -0300

    style - modifica visualização do line

--- src/theme/colors/dark.ts
+++ src/theme/colors/dark.ts
@@ -8,7 +8,7 @@ export const darkColors = {
     surface: "#1E1E1E",
     surfaceSecondary: "#2D2D2D",
     surfaceVariant: "#3D3D3D",
-    surfaceLine:"#0b1c30",
+    surfaceLine:"#f5f5f5",
     
     error: '#ba1a1a',
     surfaceContainer: "#FFFFFF",
