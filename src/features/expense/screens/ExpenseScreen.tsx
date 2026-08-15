b360105f62a954555776f7f290e2c0fcdf4f8db6
Rafael Brandao <rbs13bg13@gmail.com>
2026-08-14 22:58:52 -0300

    feat (gastos): adiciona skeleton de carregamento na tela de gastos

--- src/features/expense/screens/ExpenseScreen.tsx
+++ src/features/expense/screens/ExpenseScreen.tsx
@@ -10,10 +10,21 @@ import { Icon } from "@/components";
 import { styles as expensesStyles }  from "../styles/expense.style";
 import ExpenseSection from "../components/ExpenseSection";
 import ExpenseItem from "@/features/home/sections/ExpenseItem";
+import { useEffect, useState } from "react";
+import { SkeletonBlock } from "@/components/Skeleton";
 
 export default function ExpensesScreen() {
+  const [loading, setLoading] = useState<boolean>(true);
   const theme = useTheme();
   const styles = expensesStyles(theme);
+
+
+  useEffect(() => {
+    // Simulate loading data
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <Screen scrollable>
@@ -26,37 +37,53 @@ export default function ExpensesScreen() {
         onSearchPress={() => {alert("Search pressed")}}
       />
 
-      <SummaryCard
-        title="Total gasto no mês"
-        subTitle="R$ 4.280,50"
-        info="8% menor que mês passado"
-      >
-        <Icon
-          name="trending-down"
-          size={16}
-          color="#FFFFFF"
+      {loading ? (
+        <SkeletonBlock
+          width="100%"
+          height={130}
+          borderRadius={theme.radius.md}
         />
-      </SummaryCard>
+      ) : (
+        <SummaryCard
+          title="Total gasto no mês"
+          subTitle="R$ 4.280,50"
+          info="8% menor que mês passado"
+        >
+          <Icon
+            name="trending-down"
+            size={16}
+            color="#FFFFFF"
+          />
+        </SummaryCard>
+      )}
 
       <View style={styles.listContainer}>
-        <ExpenseSection
+        {loading ? (
+          <SkeletonBlock
+            width="100%"
+            height={130}
+            borderRadius={theme.radius.md}
+          />
+        ) : (
+          <ExpenseSection
             title="Hoje"
             total="R$ 158,00"
-        >
+          >
             <ExpenseItem
-                description="Almoço Executivo"
-                amount="-R$ 42,00"
-                date="09:30"
-                icon="restaurant"
+              description="Almoço Executivo"
+              amount="-R$ 42,00"
+              date="09:30"
+              icon="restaurant"
             />
 
             <ExpenseItem
-                description="Uber"
-                amount="-R$ 116,00"
-                date="12:10"
-                icon="directions-car"
+              description="Uber"
+              amount="-R$ 116,00"
+              date="12:10"
+              icon="directions-car"
             />
-        </ExpenseSection>
+          </ExpenseSection>
+        )}
       </View>
     </Screen>
   );
